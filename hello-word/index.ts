4afe436513199dc5a9bb00df79dbefa7468f1751
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2024-04-17 22:56:07 +0300

    feat: add hello-word

--- hello-word/index.ts
+++ hello-word/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import { Serve, type Context } from "ikari";
+import { Controller, Get } from "ikari/decorators";
+
+@Controller("/")
+class HelloWorldController {
+  @Get("/")
+  index(ctx: Context) {
+    return ctx.json({ message: "Hello World!" });
+  }
+}
+
+const server = Serve({
+  controllers: [HelloWorldController],
+});
+
+process.on("SIGINT", () => {
+  console.log("Bye bye!");
+  server.stop();
+});
