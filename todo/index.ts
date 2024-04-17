798cec86a07f4a73f86856a5ef644be545b5a472
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2024-04-17 21:31:49 +0300

    feat: add todo index.ts

--- todo/index.ts
+++ todo/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import  { Serve } from "ikari";
+import  TodoController from "./src/controller/todo";
+
+const server = Serve({
+  prefix: "/api",
+  controllers: [TodoController],
+  middlewares: [],
+});
+
+process.on("SIGINT", () => {
+  console.log("Stopping server...");
+  server.stop();
+});
+
